dcc51121f260c81c4c5137b03be2866c201d1eb5
Alysson Gabriel <alysson.gabriel61@gmail.com>
2025-06-11 10:17:47 -0400

    feat: cria backend do projeto (spring boot)

---
+++ barbersantos/src/main/java/com/rma/barbersantos/BarbersantosApplication.java
@@ -0,0 +1,13 @@
+package com.rma.barbersantos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbersantosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbersantosApplication.class, args);
+	}
+
+}
---
+++ barbersantos/src/test/java/com/rma/barbersantos/BarbersantosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rma.barbersantos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbersantosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
