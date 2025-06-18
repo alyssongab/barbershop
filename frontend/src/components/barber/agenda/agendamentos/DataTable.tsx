129e95f03811a4bcdf79cd5b9da145c96ded2c3b
Alysson Gabriel <alysson.gabriel61@gmail.com>
2025-06-18 16:34:47 -0400

    style: ajusta tamanho do texto nas tabelas (lg para base)

--- frontend/src/components/barber/agenda/agendamentos/DataTable.tsx
+++ frontend/src/components/barber/agenda/agendamentos/DataTable.tsx
@@ -58,6 +58,7 @@ export function DataTable<TData, TValue>({
               <TableRow
                 key={row.id}
                 data-state={row.getIsSelected() && "selected"}
+                className="text-base"
               >
                 {row.getVisibleCells().map((cell) => (
                   <TableCell key={cell.id}>
