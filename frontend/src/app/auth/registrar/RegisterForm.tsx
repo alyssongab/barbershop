c891003e990be79ebb62ed2a27d41e1c3b711999
Alysson Gabriel <alysson.gabriel61@gmail.com>
2025-07-06 18:05:12 -0400

    feat: cria tela de cadastro

--- frontend/src/app/auth/registrar/RegisterForm.tsx
+++ frontend/src/app/auth/registrar/RegisterForm.tsx
@@ -6,7 +6,8 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import Link from "next/link";
 
-const LoginForm = () => {
+const RegisterForm = () => {
+  const [name, setName] = useState("")
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
 
@@ -15,18 +16,31 @@ const LoginForm = () => {
       <div className="bg-[#ffffff] rounded-lg shadow-sm border border-[#cccccc] max-w-6xl w-full grid grid-cols-1 lg:grid-cols-2 overflow-hidden">
         {/* div da esquerda - imagem svg */}
         <div className="p-5 max-lg:hidden">
-          <img src="/login.svg" alt="Login illustration" />
+          <img src="/login.svg" alt="Register illustration" />
         </div>
 
-        {/* div da direita - login form */}
+        {/* div da direita - cadastro form */}
         <div className="p-8 flex flex-col justify-center">
           <div className="max-w-sm mx-auto w-full">
             <h1 className="text-2xl md:text-4xl font-bold text-[#242424] mb-8 text-center">
-              Login
+              Cadastro
               <div className="w-16 h-1 bg-[#242424] mx-auto mt-2"></div>
             </h1>
 
             <form className="space-y-6">
+              <div className="w-full">
+                <Label htmlFor="name" className="text-[#242424] text-lg font-medium">
+                  Nome
+                </Label>
+                <Input
+                  id="name"
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  className="mt-2 h-12 border-[#cccccc] rounded-lg bg-[#f2f2f2] focus:border-[#6c63ff] focus:ring-[#6c63ff]"
+                />
+              </div>
+
               <div className="w-full">
                 <Label htmlFor="email" className="text-[#242424] text-lg font-medium">
                   Email
@@ -51,27 +65,16 @@ const LoginForm = () => {
                   onChange={(e) => setPassword(e.target.value)}
                   className="mt-2 h-12 border-[#cccccc] rounded-lg bg-[#f2f2f2] focus:border-[#6c63ff] focus:ring-[#6c63ff]"
                 />
-                <button type="button" className="text-[#242424] mt-4 underline cursor-pointer">
-                  Esqueci minha senha
-                </button>
               </div>
 
-              <Link href="../app/cliente/agendamento">
+              <Link href="../auth/login">
                 <Button
                   type="button"
                   className="w-full h-12 bg-[#000000] hover:bg-[#242424] text-[#ffffff] rounded-lg text-lg font-medium mt-8 cursor-pointer"
                 >
-                    Entrar
+                  Cadastrar
                 </Button>
               </Link>
-
-              <div className="mt-3 text-end">
-                <Link href="../auth/registrar">
-                  <button type="button" className="text-[#242424] underline cursor-pointer">
-                    ou Cadastre-se
-                  </button>
-                </Link>
-              </div>
             </form>
           </div>
         </div>
@@ -80,4 +83,4 @@ const LoginForm = () => {
   )
 }
 
-export default LoginForm;
+export default RegisterForm;
---
+++ frontend/src/app/auth/registrar/page.tsx
@@ -0,0 +1,9 @@
+import RegisterForm from "./RegisterForm"
+
+const Registrar = () => {
+    return(
+        <RegisterForm />
+    )
+}
+
+export default Registrar;
